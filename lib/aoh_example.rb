484c4e144cdd9b2b48360f2aa46e7149abd4ce9f
Jared Fleming <jaredmf10@gmail.com>
2020-01-16 20:37:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,7 +32,9 @@ end
 
 def aoh_lookup(aoh, row, key)
   requested_value = aoh(row, key)
+  return requested_value
 end
+
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
